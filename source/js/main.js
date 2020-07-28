65a686a09c54bfb938f629d1ce4e978a1b867a0b
Faceoff88 <Faceoff88@gmail.com>
2020-07-28 16:05:55 +0300

    Добавил страницу камня

--- source/js/main.js
+++ source/js/main.js
@@ -70,20 +70,18 @@ var stonePopupImage = stonePopup.querySelector('.stone-popup__image');
 var stonePopupPrice = stonePopup.querySelector('.stone-popup__price');
 var stonePopupPriceOfList = stonePopup.querySelector('.stone-pupup__price-of-list');
 
-
 var getSelectedStoneInfo = function (evt) {
-  evt.preventDefault();
-
   var target = evt.target;
   stonePopupImage.src = target.parentNode.querySelector('.categories__image').src;
   stonePopupTitle.innerText = target.parentNode.querySelector('.categories__subitem-name').innerText;
   stonePopupPrice.innerText = target.parentNode.querySelector('.categories__subitem-price').innerText;
   stonePopupPriceOfList.innerText = target.dataset.priceoflist;
   if (target.classList[1] == 'popup-link') {
+    evt.preventDefault();
     stonePopup.classList.add('stone-popup--open');
     closePopup.addEventListener('click', onClosePopup);
   }
-}
+};
 
 var onClosePopup = function (evt) {
   evt.preventDefault();
